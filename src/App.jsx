ff4a4c125cc2c1323b97161585a5d4089ec30273
Daniel Wu <danielw72908@gmail.com>
2024-08-01 16:59:25 -0400

    learned how components and props work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Hello from "./components/Hello";
+
 function App() {
-  return <>
-    Hello World!
-  </>;
+  return (
+    <>
+      <Hello name="Daniel" message="What's good" />
+      <Hello name="Connor" message="Hello" />
+    </>
+  );
 }
 
 export default App;
